baf183315fa385dead6e08670b6cf718f061c10c
HHZ <1536845160@qq.com>
2023-03-12 20:21:49 +0800

    second commit

--- src/main/java/ggg.java
+++ src/main/java/ggg.java
@@ -1,5 +1,7 @@
 public class ggg {
     public static void main(String[] args) {
         System.out.println("qq黄qq");
+        System.out.println("sss");
+        System.out.println("sasd");
     }
 }
